d63ba3376c9440edfa86444c9531820aac01abef
gustavoabell <gustavoabel.contato@gmail.com>
2022-03-07 20:16:07 -0300

    ✅ feat: create component from switch theme

--- app.vue
+++ app.vue
@@ -1,24 +1,38 @@
+import "@vueform/toggle" from "app.vue"
 <template>
   <div
     :class="{
-      'theme-light': theme === 'light',
-      'theme-dark': theme === 'dark',
+      'theme-light': !darkMode,
+      'theme-dark': darkMode,
     }"
     class="h-screen bg-themeBackground p-5"
   >
     <h1 class="text-themeText">Nuxt 3 Tailwind Dark Mode Demo</h1>
-    <Toggle v-model="themeModel" off-label="Light" on-label="Dark" />
+    <Toggle v-model="darkMode" off-label="Light" on-label="Dark" />
   </div>
 </template>
 
 <script setup lang="ts">
 import Toggle from '@vueform/toggle'
-import { useTheme } from '~/composables/useTheme'
 import { useState } from '#app'
-import { watch } from '@vue/runtime-core'
-const { theme, setTheme } = useTheme()
-const themeModel = useState('theme', () => theme.value === 'dark')
-watch(themeModel, (selected) => {
+import { onMounted, watch } from '@vue/runtime-core'
+type Theme = 'light' | 'dark'
+const LOCAL_STORAGE_THEME_KEY = 'theme'
+const darkMode = useState('theme', () => false)
+const setTheme = (newTheme: Theme) => {
+  localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+  darkMode.value = newTheme === 'dark'
+}
+onMounted(() => {
+  const isDarkModePreferred = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const themeFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme
+  if (themeFromLocalStorage) {
+    setTheme(themeFromLocalStorage)
+  } else {
+    setTheme(isDarkModePreferred ? 'dark' : 'light')
+  }
+})
+watch(darkMode, (selected) => {
   setTheme(selected ? 'dark' : 'light')
 })
 </script>
---
+++ components/ThemeSwitch.vue
@@ -0,0 +1,10 @@
+<template>$END$</template>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<{ test: boolean }>(), { test: false })
+const emit = defineEmits<{
+  (e: 'change', id: number): void
+}>()
+</script>
+
+<style scoped></style>
